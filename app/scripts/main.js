ebfe6eeefb0f2f0d0a12edbf024dc9c22a7cb588
Fedor Baart <fedor.baart@deltares.nl>
2015-07-01 17:42:07 +0200

    event gui

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+/* global addMap */
+/* jshint devel:true */
+'use strict';
+
+addMap();
+$(function () {
+    $('#startDate').datetimepicker();
+    $('#endDate').datetimepicker();
+    $("#save").click(function(e){
+        // store data
+        $("#modal-form").modal('hide');
+        // get variables from the form
+        var properties =  $('#modal-form').data().feature.properties;
+        properties.startDate = $('#startDate').data("DateTimePicker").date().toJSON();
+        properties.endDate = $('#endDate').data("DateTimePicker").date().toJSON();
+
+    });
+});
